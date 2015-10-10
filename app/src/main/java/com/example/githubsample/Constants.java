aa1543b269c697bc35961da29d1eccbf9a1a41b9
Akash Khan <akashkhan25@gmail.com>
2015-10-10 16:42:32 +0530

    Modified user object

--- app/src/main/java/com/example/githubsample/Constants.java
+++ app/src/main/java/com/example/githubsample/Constants.java
@@ -6,4 +6,8 @@ package com.example.githubsample;
 public class Constants {
 
   public static final String BASE_URL = "https://api.github.com";
+
+  public static final String KEY_USER_NAME = "login";
+  public static final String KEY_USER_ID = "id";
+  public static final String KEY_USER_PROFILE_PIC_URL = "avatar_url";
 }
--- app/src/main/java/com/example/githubsample/User.java
+++ app/src/main/java/com/example/githubsample/User.java
@@ -1,5 +1,8 @@
 package com.example.githubsample;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 
 /**
@@ -7,11 +10,19 @@ import java.io.Serializable;
  */
 public class User implements Serializable {
 
+  private static final String TAG = User.class.getSimpleName();
+
   private String userName;
   private String userProfilePicUrl;
   private int userId;
 
   public User() {
+  }
+
+  public User(JSONObject userObject) throws JSONException{
+      userName = userObject.getString(Constants.KEY_USER_NAME);
+      userProfilePicUrl = userObject.getString(Constants.KEY_USER_PROFILE_PIC_URL);
+      userId = userObject.getInt(Constants.KEY_USER_ID);
   }
 
   public String getUserName() {
